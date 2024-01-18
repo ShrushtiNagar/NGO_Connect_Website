2312420905fee7303381e715ffa5d2ff01e28f1c
Shrushti Nagar <Shrushtinagar220@gmail.com>
2024-01-18 22:44:46 +0530

    First commit

---
+++ Images/ngo.js
@@ -0,0 +1,10 @@
+
+function requestAccountDeletion(){
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+    // if (email.trim() === "" && number.trim() === "") {
+    //     alert("Please enter a valid email or number.");
+    //     return;
+    // }
+    alert("Account deletion request sent. The developer will review your request.");
+}
